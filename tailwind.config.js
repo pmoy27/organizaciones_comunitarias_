4435917a489bc7f743c5fa1545f2742a1a74ca85
pmoy27 <pablo.oyarzok@gmail.com>
2024-01-26 15:20:06 -0300

    menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./src/**/*.{html,js}",
+      "static/tw/**/*.{html,js}",
       "./node_modules/flowbite/**/*.js"
   
     ],
